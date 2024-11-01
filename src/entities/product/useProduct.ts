883bd4c8b64c1c4d79eca3964842c7c90a32f2fb
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2024-11-01 08:20:09 +0300

    Добавить поддержку шрифтов Inter и Roboto; рефакторинг функций загрузки продуктов для улучшения читаемости и обработки ошибок

--- src/entities/product/useProduct.ts
+++ src/entities/product/useProduct.ts
@@ -2,6 +2,15 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Product } from "./Product.types";
 
+const fetchProductById = async (id: string): Promise<Product> => {
+  const response = await axios.get(`http://localhost:3000/products/${id}`, {
+    headers: {
+      Authorization: `Bearer token1`,
+    },
+  });
+  return response.data;
+};
+
 export const useProduct = (id: string) => {
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
@@ -10,10 +19,8 @@ export const useProduct = (id: string) => {
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:3000/products/${id}`,
-        );
-        setProduct(response.data);
+        const productData = await fetchProductById(id);
+        setProduct(productData);
       } catch (err) {
         setError("Ошибка загрузки продукта: " + err);
       } finally {
--- src/entities/product/useProducts.ts
+++ src/entities/product/useProducts.ts
@@ -2,22 +2,33 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Product } from "./Product.types";
 
+const fetchProducts = async (): Promise<Product[]> => {
+  const response = await axios.get("http://localhost:3000/products", {
+    headers: {
+      Authorization: `Bearer token1`,
+    },
+  });
+  return response.data;
+};
+
 export const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3000/products")
-      .then((response) => {
-        setProducts(response.data);
-        setLoading(false);
-      })
-      .catch((err) => {
+    const fetchData = async () => {
+      try {
+        const productsData = await fetchProducts();
+        setProducts(productsData);
+      } catch (err) {
         setError("Ошибка загрузки продуктов: " + err);
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
 
   return { products, loading, error };
