76c194141fb720670e604b18ece553ab6b576b81
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2024-11-30 15:41:40 +0300

    fix: обновить prettier до версии 3.4.1 и исправить форматирование в main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 
-const container = document.getElementById('root');
-if (!container) throw new Error('Failed to find root element');
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find root element");
 const root = createRoot(container);
 
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
-
